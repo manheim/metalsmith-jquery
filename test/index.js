3c5b910f182bbdf0f2f7e34d6bc95f57796b165c
pofallon <paul@ofallonfamily.com>
2015-04-22 13:23:48 -0400

    Working test

--- test/index.js
+++ test/index.js
@@ -1,22 +1,30 @@
 var assert = require('assert');
 var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
-var cheerio = require('..');
+var jquery = require('..');
+var cheerio = require('cheerio');
 
-describe('metalsmith-cheerio', function() {
+describe('metalsmith-jquery', function() {
 
 	it('should apply changes to HTML', function(done) {
 	  var metalsmith = Metalsmith('test/fixtures/basic');
 	  metalsmith
 	  .use(markdown())
-      .use(cheerio(function($) {
+      .use(jquery(function($) {
           $('h2').addClass('welcome');
       }))
-      .build(function(err) {
+      .build(function(err, files) {
           if (err) {
             return(done(err));
           } else {
-            return(done());
+            var count = 0;
+            Object.keys(files).forEach(function(file) {
+              $ = cheerio.load(files[file].contents);
+              $('h2').each(function() {
+                assert.equal(true,$(this).hasClass('welcome'));
+              });
+            });
+            done();
           }
       });
 	});
